9dbfae195ce3331b8845323d4737429d6bcae2b3
elidmyriam <elidmyriam513@gmail.com>
2024-10-02 20:28:37 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/firstpractice/MainActivity.kt
+++ app/src/main/java/com/example/firstpractice/MainActivity.kt
@@ -31,7 +31,7 @@ import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.firstpractice.ui.theme.FirstPracticeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
